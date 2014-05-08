1d96acac592e2c0814d6a30582568e588d407386
Nathan Harris <nathanrharris@gmail.com>
2014-05-08 11:58:40 -0500

    added checks for non existing profiles

--- sites/all/themes/myngl/templates/user-profile.tpl.php
+++ sites/all/themes/myngl/templates/user-profile.tpl.php
@@ -2,15 +2,19 @@
   $uid = arg(1);
   $u = entity_metadata_wrapper('user', user_load($uid));
   $profile = profile2_load_by_user($uid, 'profile');
-  if (isset($profile)) {
+  if (isset($profile->pid)) {
     $p = entity_metadata_wrapper('profile2', $profile);
   }
 ?>
 <div class="profile"<?php print $attributes; ?>>
   <div id="content-left">
     <?php
-      $pic = $p->field_picture->value();
-      print theme_image_style(array('style_name' => 'user_profile_circle_image', 'path' => $pic['uri'], 'attributes' => array('class' => 'profile-pic'), 'height' => null, 'width' => null));
+      if (isset($p)) { 
+        $pic = $p->field_picture->value();
+        print theme_image_style(array('style_name' => 'user_profile_circle_image', 'path' => $pic['uri'], 'attributes' => array('class' => 'profile-pic'), 'height' => null, 'width' => null));
+      } else {
+        print theme_image_style(array('style_name' => 'user_profile_circle_image', 'path' => 'public://default-user-profile.png', 'attributes' => array('class' => 'profile-pic'), 'height' => null, 'width' => null));
+      }
     ?>
     <div id="profile-points">
       <div id="profile-points-total"><p class="points">545</p></div>
@@ -24,22 +28,24 @@
       <?php print $u->field_first_name->value().' '.$u->field_last_name->value(); ?><br />
       </strong>
 
-      <span class="label">LOCATION:</span>
-      <?php print $p->field_city->value(); ?><br />
-      
-      <em><?php print $p->field_about->value(); ?></em><br />
-      
-      <span class="label">MY INTERESTS:</span>  
-      <span class="interests">
-      <?php 
-        $interests = array();
-        foreach ($p->field_interests->value() as $i)  {
-          $interests[] = $i->name;
-        }
+      <?php if (isset($p)) : ?>
+        <span class="label">LOCATION:</span>
+        <?php print $p->field_city->value(); ?><br />
+        
+        <em><?php print $p->field_about->value(); ?></em><br />
+        
+        <span class="label">MY INTERESTS:</span>  
+        <span class="interests">
+        <?php 
+            $interests = array();
+            foreach ($p->field_interests->value() as $i)  {
+              $interests[] = $i->name;
+            }
 
-        print join(', ', $interests);
-      ?>
-      </span>
+            print join(', ', $interests);
+        ?>
+        </span>
+      <?php endif; ?>
     </div>
     <div id="profile-menu">
       <div class="border-bottom">
