82ea659dae7e4ddd3f0629577d34f6f759963d98
Nathan Harris <nathanrharris@gmail.com>
2014-05-13 16:12:36 -0500

    set up color variables for html template

--- sites/all/themes/myngl/template.php
+++ sites/all/themes/myngl/template.php
@@ -2,6 +2,18 @@
 
 drupal_add_library('system', 'jquery.cookie');
 
+function myngl_preprocess_html(&$vars) {
+  if (arg(0) == 'myngl-event') {
+    $myngl = node_load(arg(1));
+    $brand = node_load($myngl->field_myngl_brand['und'][0]['nid']);
+
+    $vars['colors']['primary'] = $brand->field_brand_primary_color['und'][0]['rgb'] ;
+    $vars['colors']['secondary'] = $brand->field_brand_secondary_color['und'][0]['rgb'] ;
+    $vars['colors']['background'] = $brand->field_brand_background_color['und'][0]['rgb'] ;
+    $vars['colors']['tertiary'] = $brand->field_brand_tertiary_color['und'][0]['rgb'] ;
+  }
+}
+
 function myngl_preprocess_page(&$vars) {
   if (arg(0) == 'myngl-event') {
     $myngl = node_load(arg(1));
