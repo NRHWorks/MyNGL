fe86104fce45aa2baed80e4b720fc1cca670f9b0
Nathan Harris <nathan@nathanrharris.com>
2014-07-05 00:07:54 -0500

    adding exit page

--- sites/all/themes/myngl/templates/footer.php
+++ sites/all/themes/myngl/templates/footer.php
@@ -6,7 +6,7 @@
           <li class="inactive" id="theater"><a href="/myngl-event/<?php print $myngl->nid; ?>/theater">Theater</a></li>
           <li class="inactive" id="play-room"><a href="/myngl-event/<?php print $myngl->nid; ?>/activity-room">PlayRoom</a></li>
           <li class="inactive" id="gifting-suite"><a href="/myngl-event/<?php print $myngl->nid; ?>/rewards">Gifting Suite</a></li>
-          <li class="inactive" id="exit"><a href="/user">Exit</a></li>
+          <li class="inactive" id="exit"><a href="/myngl-event/<?php print $myngl->nid; ?>/exit">Exit</a></li>
           <li class="inactive" id="help"><a href="#" onclick="myngl.overlay('help-overlay',500,800);">HELP</a></li>
           <li class="inactive" id="pause"><a href="#"><i class="fa fa-pause"></i></a></li>
           <li class="inactive" id="the-myngl"><a href="#">The Myngl</a></li>
---
+++ sites/all/themes/myngl/templates/myngl-event-exit.tpl.php
@@ -0,0 +1,19 @@
+<div id="exit-div">
+  <img src="/sites/all/themes/myngl/images/logo.png" /><br /><br />
+
+  <span class="exit-thank-you">THANK YOU FOR JOINING US</span><br /><br />
+
+  <div id="exit-points">
+    <br />You Have<br />
+    <div id="exit-points-circle">
+      <span class="exit-points-points"><?php print $points; ?></span><br />
+      Points
+    </div>
+  </div>
+
+
+  <div id="exit-dashboard-link">
+    <div class="exit-circle"><a href="/">&gt;</a></div>
+    <span class="exit-back">BACK TO THE DASHBOARD</span> 
+  </div>
+</div>
