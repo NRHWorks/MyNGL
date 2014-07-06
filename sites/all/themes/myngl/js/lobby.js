a60f433a61b6b119f2e71401b08c88fe5e50fa56
Nathan Harris <nathan@nathanrharris.com>
2014-07-05 22:30:40 -0500

    lobby should be good to go now

--- sites/all/themes/myngl/js/lobby.js
+++ sites/all/themes/myngl/js/lobby.js
@@ -2,18 +2,13 @@ var apiready=0;
 var intervalid;
 var player;
 
-function onYouTubeIframeAPIReady() {
-  apiready= 1;
-}
-
 (function ($) {
   $(document).ready( function() {
     myngl.update_participant_status(Drupal.settings.myngl_id, Drupal.settings.user_id,"Lobby");
     update_status_interval = setInterval(function(){myngl.update_participant_status(Drupal.settings.myngl_id, Drupal.settings.user_id,"Lobby");},20000);
 
-    intervalid = setInterval(function() { youtube.loadvideo(); }, 500);
     if (jQuery.cookie('done_lobby_video') !='1') {
-/* TODO: need to get this fixed in safari, and firefox 
+
       $(".branded ul li#lounge").removeClass('inactive');
       $(".branded ul li#theater").removeClass('inactive');
       $(".branded ul li#play-room").removeClass('inactive');
@@ -24,29 +19,9 @@ function onYouTubeIframeAPIReady() {
       $(".branded ul li#play-room a").attr("onclick", "return false;").css("cursor", "default");
       $(".branded ul li#gifting-suite a").attr("onclick", "return false;").css("cursor", "default");
       $("iframe#youtube-field-player").attr('src',$("iframe#youtube-field-player").attr('src') + '&enablejsapi=1');
-*/
     }
-  });
-}(jQuery));
 
-var youtube = (function ($) {
-  return {
-    loadvideo: function(){
-      if (apiready==1) {
-        player = new YT.Player('youtube-field-player', {
-          events: {
-          'onStateChange':youtube.onPlayerStateChange
-          }
-        });
-        clearInterval(intervalid);
-      }
-
-    },
-
-    onPlayerStateChange: function(evt) {
-
-      if (evt.data == YT.PlayerState.ENDED ) {
-
+    $('video').bind('ended',  function(){
         jQuery.cookie('done_lobby_video', '1');
 
         $(".branded ul li#lounge").addClass('inactive');
@@ -64,7 +39,7 @@ var youtube = (function ($) {
           var social_area = window.location.href.replace("lobby", "social-area");
           window.location.href = social_area;
         }  ,2000);
-      }
-    },
-  }
-}(jQuery));
+    });
+
+  });
+})(jQuery);
--- sites/all/themes/myngl/templates/myngl-event-social-area.tpl.php
+++ sites/all/themes/myngl/templates/myngl-event-social-area.tpl.php
@@ -1,4 +1,13 @@
 <?php
+  if (!isset($_COOKIE['done_lobby_video']) || $_COOKIE['done_lobby_video']!= 1){
+    global $base_url;
+    $redirect = 'Location: '. $base_url . '/myngl-event/' . $myngl->nid ."/lobby";
+    header($redirect);
+    exit;
+  }
+?>
+
+<?php
   global $user;
   $primary_color = $brand->field_brand_primary_color['und'][0]['rgb'];
   $secondary_color = $brand->field_brand_secondary_color['und'][0]['rgb'];
