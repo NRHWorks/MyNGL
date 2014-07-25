66ba50b9d951f6a4c57c0bbbbcd76ee8d803383f
Jarod Smith <jarodms@gmail.com>
2014-07-25 14:58:00 -0500

    #77 - increase screen 30%

--- sites/all/themes/myngl/templates/myngl-event-lobby.tpl.php
+++ sites/all/themes/myngl/templates/myngl-event-lobby.tpl.php
@@ -1,7 +1,7 @@
 
 <div class="video-container">
   <div>
-    <video width="650px" height="350px" autoplay>
+    <video width="845px" height="455px" autoplay>
       <source src="http:<?php print str_replace('+','%2B',file_create_url($myngl->field_welcome_video['und'][0]['uri'])); ?>" type="video/mp4">
       Your browser does not support the video tag.
     </video>
