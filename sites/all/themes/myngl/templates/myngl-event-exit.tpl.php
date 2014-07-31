7df612b4fc61d62d50794dbcb20cee4b8a9ae5ed
Ming-ching Chiu <percle@gmail.com>
2014-07-31 06:40:10 +0000

    ticket 96

--- sites/all/themes/myngl/templates/myngl-event-exit.tpl.php
+++ sites/all/themes/myngl/templates/myngl-event-exit.tpl.php
@@ -1,7 +1,12 @@
-<div id="exit-div" style="height:200px;padding-top:100px">
-  <img src="/sites/all/themes/myngl/images/logo.png" /><br /><br />
 
+
+<div id="exit-div" style="padding-top:100px; padding-bottom:100px;">
+  <img src="/sites/all/themes/myngl/images/logo.png" /><br /><br />
   <span class="exit-thank-you">THANK YOU FOR JOINING US</span><br /><br />
+  <?php //$exit_text = field_view_field('node', $myngl, 'field_exit_text','full' ); //Not sure why this line doesn't work.. ?>
+  <div class='exit-text' style="margin:50px 50px 80px 50px;">
+    <?php print $myngl->field_exit_text['und'][0]['safe_value']; ?>
+  </div>
   <!--
   <div id="exit-points">
     <br />You Have<br />
