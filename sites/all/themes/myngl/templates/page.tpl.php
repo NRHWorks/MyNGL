bbf8994bcb9a107e6e9ca11a7417bc38705d7ae8
Ming-ching Chiu <percle@gmail.com>
2014-08-25 19:10:39 +0000

    ticket 131

--- sites/all/themes/myngl/templates/page.tpl.php
+++ sites/all/themes/myngl/templates/page.tpl.php
@@ -54,9 +54,14 @@
 
   <div id="terms-overlay" class="overlay">
     <h1>Terms and Conditions</h1>
-    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque, massa in viverra ornare, lorem lectus scelerisque elit, sed pharetra velit diam eget diam. Duis in sagittis mauris. Aenean euismod vestibulum neque. Phasellus pretium vitae ligula vel sagittis. Quisque libero eros, laoreet id vestibulum non, pretium quis urna. Aenean elit magna, ultricies faucibus lobortis vitae, rhoncus quis nibh. In in massa vitae leo feugiat tincidunt. Sed ornare nisl nec ante blandit, id vehicula neque rutrum. Pellentesque euismod, magna nec condimentum dapibus, eros turpis semper urna, ac tincidunt lectus justo vitae urna. Donec cursus dapibus convallis. Nunc imperdiet pretium malesuada.  </p>
-    <p> Cras dignissim porta lorem vitae posuere. Mauris vitae tristique risus. Curabitur arcu lacus, iaculis ac malesuada et, eleifend eget purus. Morbi in gravida nisi. Vestibulum turpis sapien, fringilla ac pretium ut, porta nec justo. Nullam dignissim nisl vel vulputate iaculis. In pretium arcu et magna tempor, eu laoreet ipsum fermentum. Fusce semper lacinia eros, quis laoreet massa ultrices sit amet. In ex elit, volutpat porta diam sed, facilisis lobortis augue. Ut porttitor mauris enim, vel vestibulum nunc aliquet sit amet. Sed in sapien vel augue aliquam porttitor. Fusce sit amet venenatis velit. Mauris finibus ex malesuada metus maximus, non ultricies turpis imperdiet.  </p>
-    <p> Vivamus in quam dui. Etiam facilisis hendrerit commodo. Mauris semper, tortor ac blandit iaculis, ipsum risus efficitur purus, eu cursus elit massa commodo purus. Vestibulum elit felis, porta vel dolor vitae, euismod placerat urna. Nunc arcu nulla, tristique eget purus mollis, gravida sagittis neque. Aenean vel justo quis lacus laoreet maximus ac et eros. Ut a scelerisque purus. Integer non pretium est, sit amet varius urna. Aenean vitae diam semper, tincidunt augue sed, pretium augue. Sed a pellentesque elit. Donec sed urna varius, fringilla tortor et, dapibus elit. Integer et dui sapien. Nulla nec diam quis elit bibendum placerat convallis eget tellus. Nulla euismod nulla sed nisl lacinia luctus. Curabitur imperdiet justo id dolor placerat pulvinar. Cras feugiat ipsum id ex ullamcorper malesuada.  </p>
+      <div style="width:760px; height:400px; overflow: scroll">
+      <?php
+        $block = module_invoke('block','block_view','2');
+        print render($block['content']);
+      ?>
+      </div>
+
+
     <a href="#" style="float:right; margin-top:25px" onclick="myngl.terms_overlay_close();">Close</a>
   </div>
 
